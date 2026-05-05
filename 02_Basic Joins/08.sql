c1fb9ba4d68d25e6db7815f73e2d5d52a23db186
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-05 21:42:48 +0530

    08

---
+++ 02_Basic Joins/08.sql
@@ -0,0 +1,14 @@
+--Customer Who Visited but Did Not Make Any Transactions
+/*
+https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+*/
+
+/*
+I use COUNT(*) because I want to count rows. Using COUNT(column) depends on NULLs, so COUNT(*) is safer and clearer.
+*/
+select v.customer_id , count(*) as count_no_trans
+from Visits v
+left join transactions t
+on v.visit_id = t.visit_id
+where t.transaction_id is NULL
+group by v.customer_id;
