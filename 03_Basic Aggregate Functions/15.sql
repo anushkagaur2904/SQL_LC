760faf69d359e363b46c8eeccdc07f87f04aab67
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-12 12:05:43 +0530

    15

---
+++ 03_Basic Aggregate Functions/15.sql
@@ -0,0 +1,10 @@
+-- Not Boring Movies
+/*
+https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+*/
+
+select *
+from Cinema 
+where id%2=1
+and description!='boring'
+order by rating desc;
