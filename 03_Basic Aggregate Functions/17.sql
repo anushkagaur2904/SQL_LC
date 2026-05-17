7728723632ed5bb560453a3f1b7b3894ce064bf5
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-17 12:33:40 +0530

    17

---
+++ 03_Basic Aggregate Functions/17.sql
@@ -0,0 +1,11 @@
+-- Project Employees I
+/*
+https://leetcode.com/problems/project-employees-i/description/?envType=study-plan-v2&envId=top-sql-50
+*/
+select p.project_id,
+round(avg(e.experience_years),2) as average_years
+
+from Project p
+join Employee e
+on p.employee_id = e.employee_id
+group by p.project_id;
