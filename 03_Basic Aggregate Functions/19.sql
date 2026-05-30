200c2b2163401492f9fb4f6a52fbcf8b1e95f671
Anushka Gaur <anushkagaur52@gmail.com>
2026-05-30 18:03:09 +0530

    19

---
+++ 03_Basic Aggregate Functions/19.sql
@@ -0,0 +1,18 @@
+-- Queries Quality and Percentage
+/*https://leetcode.com/problems/queries-quality-and-percentage/description/?envType=study-plan-v2&envId=top-sql-50*/
+
+/*
+SELECT
+    query_name,
+
+    ROUND(AVG(rating / position),2) AS quality,
+
+    ROUND(
+        AVG(IF(rating < 3, 100, 0)),
+        2
+    ) AS poor_query_percentage
+
+FROM Queries
+
+GROUP BY query_name;
+*/
